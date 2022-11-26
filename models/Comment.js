f20087dc5c8b188b5edad6ee8e87be1b18396454
Richie Lollis <richiedlollis@gmail.com>
2022-11-25 19:52:35 -0500

    add validation

--- models/Comment.js
+++ models/Comment.js
@@ -3,15 +3,19 @@ const dateFormat = require("../utils/dateFormat");
 
 const ReplySchema = new Schema(
   {
+    // set custom id to avoid confusion with parent comment _id
     replyId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId(),
     },
     replyBody: {
       type: String,
+      required: true,
+      trim: true,
     },
     writtenBy: {
       type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
@@ -30,9 +34,11 @@ const CommentSchema = new Schema(
   {
     writtenBy: {
       type: String,
+      required: true,
     },
     commentBody: {
       type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
